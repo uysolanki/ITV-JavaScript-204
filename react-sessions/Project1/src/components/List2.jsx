50bb3d8b7efbad5d5896dd0b0355527334d9df34
Umar Solanki <uysolanki@gmail.com>
2025-02-19 16:54:05 +0530

    react day 3

--- react-sessions/Project1/src/components/List2.jsx
+++ react-sessions/Project1/src/components/List2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const List2 = () => {
 
-    const cars=["Audi","BMW","Merc","Honda","Tesla"]
+    const cars=["Audi","BMW","Merc","Honda","Tesla"]  //step1 : get the data
 
     const players=[
         {
@@ -21,11 +21,15 @@ const List2 = () => {
 
     function showTeamName(playerName)
     {
-        console.log(playerName)
+        switch(playerName)
+        {
+            case "Virat" : console.log("RCB"); break;
+        }
+        
     }
   return (
     <ol>
-        {cars.map(
+        {cars.map(                                                  //Step 2 : prepare the UI
             (car,index)=>{
                 return <li key={index}>{car}</li>
             }
@@ -33,7 +37,7 @@ const List2 = () => {
 
         {
             players.map(
-                (player,index)=>{
+                (player,index)=>{                                   //Step 3: make the UI Functional
                         return<h1 onClick={()=>showTeamName(player.pname)} key={index}>{player.pname}</h1>
                 }
             )
